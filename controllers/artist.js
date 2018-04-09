471098752a16d98b4e48faa972cfe1d4a4c40712
Christopher Vicuña <christopher.vicuna@gmail.com>
2018-04-09 00:07:57 -0300

    se agrega metodo getArtists paginados

--- controllers/artist.js
+++ controllers/artist.js
@@ -2,6 +2,7 @@
 
 const fs = require('fs')
 const path= require('path')
+const mongoosePagination = require('mongoose-pagination')
 
 const Artist = require('../models/artist')
 const Album = require('../models/album')
@@ -22,6 +23,32 @@ function getArtist(req, res){
         }
     })
     
+}
+
+function getArtists(req, res){
+    if(req.params.page){
+        var page = req.params.page
+    }else{
+        var page = 1
+    }
+
+    var itemsPerPage = 3
+
+    Artist.find().sort('name').paginate(page, itemsPerPage, (err, artists, total) => {
+        if(err){
+            res.status(500).send('error en la peticion de artistas')
+        }else{
+            if(!artists){
+                res.status(404).send('no se encontraron artistas')
+            }else{
+                return res.status(200).send({
+                    total: total,
+                    artists: artists 
+                })
+            }
+        }
+    })
+
 }
 
 function saveArtist(req, res) {
@@ -46,5 +73,6 @@ function saveArtist(req, res) {
 
 module.exports = {
     getArtist,
-    saveArtist
+    saveArtist,
+    getArtists
 }
--- routes/artist.js
+++ routes/artist.js
@@ -8,5 +8,6 @@ var api = express.Router()
 
 api.get('/artist/:id', middleware.ensureAuth, ArtistController.getArtist)
 api.post('/artist', middleware.ensureAuth, ArtistController.saveArtist)
+api.get('/artists/:page?', middleware.ensureAuth, ArtistController.getArtists)
 
 module.exports = api
