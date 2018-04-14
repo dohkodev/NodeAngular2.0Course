32dd8ef1baa8130fba4f25c2d5202c1389338724
Christopher Vicuña <christopher.vicuna@gmail.com>
2018-04-14 02:56:09 -0300

    agregando estructura y modelos al front para la gestion de datos con el back

---
+++ client/src/app/models/album.ts
@@ -0,0 +1,9 @@
+export class Album {
+    constructor(
+        public name: String,
+        public description: String,
+        public year: Number,
+        public image: String,
+        public artist: String
+    ){}
+}
---
+++ client/src/app/models/artist.ts
@@ -0,0 +1,8 @@
+export class Artist {
+    constructor(
+        public title: String,
+        public description: String,
+        public year: Number,
+        public image: String
+    ){}
+}
