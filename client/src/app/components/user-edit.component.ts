6f25393e6eef87cb8b053d794ed8ec30bdda428e
Christopher Vicuña <christopher.vicuna@gmail.com>
2018-04-20 11:50:13 -0300

    subiendo archivos al servidor desde el front

--- client/src/app/components/user-edit.component.ts
+++ client/src/app/components/user-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { GLOBAL } from '../services/global'
 import { UserService } from '../services/user.service'
 import { User } from '../models/user'
 
@@ -15,6 +16,7 @@ export class UserEditComponent implements OnInit {
     public identity
     public token
     public alertMessage
+    public url: string
 
     public constructor(
         private _userService: UserService
@@ -25,7 +27,7 @@ export class UserEditComponent implements OnInit {
         this.token = this._userService.getToken()
 
         this.user = this.identity
-        
+        this.url = GLOBAL.url
     }
 
     ngOnInit(){
@@ -41,7 +43,18 @@ export class UserEditComponent implements OnInit {
                     //this.user = response.user
                     localStorage.setItem('identity', JSON.stringify(this.user))
                     document.getElementById("indentityName").innerHTML = this.user.name
-                    this.alertMessage = 'Datos actualizados correctamente!'
+                    if (!this.filesToUpload){
+                        //redireccion
+                    }else{
+                        this.makeFileRequest(this.url+'upload-image-user/'+this.user._id, [], this.filesToUpload)
+                            .then((result: any) => {
+                                    this.user.image = result.image
+                                    localStorage.setItem('identity', JSON.stringify(this.user))
+                                    console.log(this.user)
+                                }
+                            )
+                    }
+                    this.alertMessage = 'Datos actualizados correctamente!'                    
                 }
             },
             error => {
@@ -53,5 +66,39 @@ export class UserEditComponent implements OnInit {
                 }
             }
         )  
+    }
+
+    public filesToUpload: Array<File>
+
+    fileChangeEvent(fileInput: any){
+        this.filesToUpload = <Array<File>>fileInput.target.files
+        //console.log(this.filesToUpload)
+    }
+
+    makeFileRequest(url: string, params: Array<string>, files: Array<File>){
+        let token = this.token
+
+        return new Promise((resolve, reject) => {
+            let formData: any = new FormData()
+            let xhr = new XMLHttpRequest
+
+            for (var i = 0; i < files.length; i++){
+                formData.append('image', files[i], files[i].name)
+            }
+
+            xhr.onreadystatechange = () => {
+                if(xhr.readyState == 4){
+                    if(xhr.status == 200){
+                    resolve(JSON.parse(xhr.response))                        
+                    }else{
+                        reject(xhr.response)
+                    }
+                }
+            }
+
+            xhr.open('POST', url, true)
+            xhr.setRequestHeader('Authorization', token)
+            xhr.send(formData)
+        })
     }
 }
